b2aa697d79bc2fd97b48eefb067ac2e2326538ad
AlexanderOsharov <sashosharov@gmail.com>
2025-03-25 22:11:22 +0300

    RandomForest

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="random_forest",
+    version="0.1",
+    author="Alexander Osharov",
+    author_email="sashosharov@gmail.com",
+    description="A simple RandomForest implementation in Python",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
